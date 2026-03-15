c0389111baa07d3d69b2fe6504a1a01948276693
ctix1 <164958544+ctix1@users.noreply.github.com>
2026-03-16 01:01:48 +0300

    Add files via upload

--- panda.config.ts
+++ panda.config.ts
@@ -1,30 +1,30 @@
 import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
-    // Whether to use postcss or not
-    postcss: true,
+  // Whether to use postcss or not
+  postcss: true,
 
-    // Where to look for your css declarations
-    include: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  // Where to look for your css declarations
+  include: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
 
-    // Files to exclude
-    exclude: [],
+  // Files to exclude
+  exclude: [],
 
-    // Useful for theme customization
-    theme: {
-        extend: {
-            tokens: {
-                colors: {
-                    brand: {
-                        primary: { value: "#6d28d9" },
-                        secondary: { value: "#7c3aed" },
-                        light: { value: "#ede9fe" },
-                    }
-                }
-            }
-        },
+  // Useful for theme customization
+  theme: {
+    extend: {
+      tokens: {
+        colors: {
+          brand: {
+            primary: { value: "#6d28d9" },
+            secondary: { value: "#7c3aed" },
+            light: { value: "#ede9fe" },
+          }
+        }
+      }
     },
+  },
 
-    // The output directory for your css system
-    outdir: "styled-system",
+  // The output directory for your css system
+  outdir: "styled-system",
 });
