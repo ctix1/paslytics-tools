8d7fa7d5993922606487ed025cfc7aea77baf534
ctix1 <164958544+ctix1@users.noreply.github.com>
2026-03-16 07:28:12 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import Pricing from './pages/Pricing';
+import Plan from './pages/Plan';
 import AboutPage from './pages/AboutPage';
 import Checkout from './pages/Checkout';
 import DashboardLayout from './components/DashboardLayout';
@@ -71,7 +71,7 @@ function App() {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
-                <Route path="/plan" element={<Pricing />} />
+                <Route path="/plan" element={<Plan />} />
                 <Route path="/about" element={<AboutPage />} />
                 
                 {/* Protected Routes */}
