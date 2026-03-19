a9d32d3c8593d561ebb142fb805e17083a26662e
ctix1 <164958544+ctix1@users.noreply.github.com>
2026-03-19 09:18:45 +0300

    Update gemini.ts

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
+console.log("check API key:", import.meta.env.VITE_GEMINI_API_KEY ? "loaded" : "missing")
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
