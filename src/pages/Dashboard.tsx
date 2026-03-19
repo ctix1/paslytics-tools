7f0c62c3d70fc17ef8a990f2de8f1207f54e1ed0
ctix1 <164958544+ctix1@users.noreply.github.com>
2026-03-20 01:51:32 +0300

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import{analyzemarketing} from '../lib/gemini';
+import{analyzeMarketing} from '../lib/gemini';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useLanguage } from '../i18n/LanguageContext';
